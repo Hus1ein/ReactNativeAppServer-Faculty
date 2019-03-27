2598c323f742b564d777af4ba2293baae6c0123f
Hussain Abdelilah <hussain.abdelilah@hotmail.com>
2019-03-27 20:46:31 +0100

    First commit

---
+++ routes/location.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/', function(req, res, next) {
+
+    fs.readFile('Database.json', 'utf8', function readFileCallback(err, data){
+        let locations;
+        if (err){
+            console.log(err);
+        } else {
+            locations = JSON.parse(data);
+            res.send(locations);
+            console.log(data);
+        }});
+});
+
+router.post('/', function(req, res, next) {
+
+    fs.readFile('Database.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let newLocation = req.body;
+            let locations = JSON.parse(data); //now it an object
+            locations.push(newLocation); //add some data
+            let locationsAsString = JSON.stringify(locations); //convert it back to json
+            fs.writeFile('Database.json', locationsAsString, 'utf8', () => {
+                res.send(newLocation);
+            }); // write it back
+        }});
+});
+
+module.exports = router;
